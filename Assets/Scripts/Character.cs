fa34a61077097ff9b7b8faa9e68222600d1e073b
Ben Bordelon <bbord32@lsu.edu>
2021-04-05 15:52:56 -0500

    Changes
    added created planet movement, create planet button, and add to orbit button

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -15,14 +15,10 @@ public class Character : MonoBehaviour
             DontDestroyOnLoad(instance);
         }
     }
-    private void Update()
+    public void AddtoOrbitButton()
     {
-       if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SceneManager.LoadScene("Solar System");
-            instance.transform.position = new Vector3(10000, 0, 0);
-            instance.transform.localScale = new Vector3(600, 600, 600); 
-
-        }
+       SceneManager.LoadScene("Solar System");
+       instance.transform.position = new Vector3(10000, 0, 0);
+       instance.transform.localScale = new Vector3(600, 600, 600);  
     }
 }
---
+++ Assets/createPlanet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class createPlanet : MonoBehaviour
+{
+    public void createPlanetButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
